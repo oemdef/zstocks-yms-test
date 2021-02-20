8a7c91c2c86a9fc42ed1d229a5dce54683efa621
OemDef <oemdef@outlook.com>
2021-02-20 16:20:05 +0300

    Initial Commit

---
+++ zStocks/zStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  zStocksApp.swift
+//  zStocks
+//
+//  Created by OemDef | HansaDev on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct zStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
